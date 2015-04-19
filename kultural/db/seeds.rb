65aed7486765b6fdbcfe0de5fa40082f55893dc9
vrdragicevic <vrdragicevic@gmail.com>
2015-04-19 12:13:57 -0300

    Add event types to seeds.rb

--- kultural/db/seeds.rb
+++ kultural/db/seeds.rb
@@ -1,7 +1,9 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rake db:seed (or created alongside the db with db:setup).
-#
-# Examples:
-#
-#   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
-#   Mayor.create(name: 'Emanuel', city: cities.first)
+event_types_list = [
+  [ "Music", "Concerts, street bands, etc" ],
+  [ "Fair", "Book fairs, etc" ],
+  [ "Art", "Expositions, paintings sale, etc" ],
+]
+
+event_types_list.each do |name, examples|
+  Country.create( name: name, examples: examples )
+end
