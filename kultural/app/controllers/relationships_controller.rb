0d1af4f864dd5d1d7b1e54b018b56f997751aca8
Jaime Castro Retamal <jecastro.ie@gmail.com>
2015-05-09 05:11:23 -0300

    Following button working

--- kultural/app/controllers/relationships_controller.rb
+++ kultural/app/controllers/relationships_controller.rb
@@ -1,2 +1,15 @@
 class RelationshipsController < ApplicationController
+
+  def create
+    user = User.find(params[:followed_id])
+    current_user.follow(user)
+    redirect_to user
+  end
+
+  def destroy
+    user = Relationship.find_by(id: params[:id]).followed
+    current_user.unfollow(user)
+    redirect_to user
+  end
+
 end
