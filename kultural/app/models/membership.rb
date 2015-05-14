ae110652260f8b8185a41ea6fc50a453761f9faf
Jaime Castro Retamal <jecastro.ie@gmail.com>
2015-05-14 02:29:48 -0300

    Gem annotate

--- kultural/app/models/membership.rb
+++ kultural/app/models/membership.rb
@@ -7,6 +7,7 @@
 #  group_id   :integer
 #  created_at :datetime         not null
 #  updated_at :datetime         not null
+#  id         :integer          not null, primary key
 #
 
 class Membership < ActiveRecord::Base
