496c32f948805a5d3b0c4ab31bf59f50ff6239c4
SASalataRT <SA.SalataRT@GMail.com>
2015-05-17 20:56:24 -0300

    finish first 2 authentication tests

--- kultural/spec/requests/authentication_spec.rb
+++ kultural/spec/requests/authentication_spec.rb
@@ -5,34 +5,38 @@ describe 'Authentication' do
   subject { page }
 
   before {
-    @user = User.create(name: 'Jaime Castro R',
-                        password: 'macoy123',
-                        password_confirmation: 'macoy123',
-                        mail: 'jecastro1@uc.cl',
-                        phone: 123456789,
-                        birthday: Date.strptime('31/12/1994', '%d/%m/%Y'),
-                        male: true)
+    @user = User.create(name: 'Napoleon Bonaparte',
+                              password: 'Austerlitz',
+                              password_confirmation: 'Austerlitz',
+                              mail: 'napoleon@uc.cl',
+                              phone: 17691821,
+                              birthday: Date.strptime('15/08/1769', '%d/%m/%Y'),
+                              male: true)
   }
 
   describe 'login' do
     before { visit login_path }
 
     describe 'with invalid credentials' do
-      # fill_in 'session_mail',     with: @user.mail
-      # fill_in 'session_password', with: 'invalidpassword'
-      before { click_button 'login-submit' }
+      it 'should fail' do
+        fill_in 'session_mail',     with: @user.mail
+        fill_in 'session_password', with: 'invalidpassword'
+        click_button 'login-submit'
 
-      it { should have_selector('div', id: 'alert') }
-      it { should have_selector('form', id: 'login-form') }
+        should have_content('Error')
+        should have_content('LOGIN')
+      end
     end
 
     describe 'with valid credentials' do
-      # fill_in 'session_mail',     with: @user.mail
-      # fill_in 'session_password', with: @user.password
-      before { click_button 'login-submit' }
+      it 'should succeed' do
+        fill_in 'session_mail',     with: @user.mail
+        fill_in 'session_password', with: @user.password
+        click_button 'login-submit'
 
-      it { should have_selector('div', id: 'notice') }
-      it { should have_selector('div', id: 'user-info') }
+        should have_content('Welcome')
+        should have_content(@username)
+      end
     end
   end
 end
