5a0ec7343782827ab8393bca31a517a3e5cff714
vrdragicevic <vrdragicevic@gmail.com>
2015-05-18 20:53:47 -0300

    Basic google map at main.html.erb

--- kultural/app/assets/javascripts/application.js
+++ kultural/app/assets/javascripts/application.js
@@ -14,4 +14,6 @@
 //= require jquery_ujs
 //= require jquery.turbolinks
 //= require turbolinks
+//= require underscore-min
+//= require gmaps/google
 //= require_tree .
--- kultural/app/controllers/pages_controller.rb
+++ kultural/app/controllers/pages_controller.rb
@@ -1,6 +1,14 @@
 class PagesController < ApplicationController
   def main
     redirect_to login_path unless logged_in?
+    @places = Place.joins(:events) #.group('places.id').having('count(place_id)>0')
+    @hash = Gmaps4rails.build_markers(@places) do |place, marker|
+      #TODO: cambiar a solo eventos futuros.
+      events = place.events
+      marker.lat place.x
+      marker.lng place.y
+      marker.infowindow render_to_string partial: 'events/list_events', locals: {events: events}
+    end
   end
 
   def help
