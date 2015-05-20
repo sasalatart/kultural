0747423087e0b35ea51765f85bfc66825040de68
vrdragicevic <vrdragicevic@gmail.com>
2015-05-19 21:39:46 -0300

    Add some seeds

--- kultural/db/seeds.rb
+++ kultural/db/seeds.rb
@@ -53,19 +53,41 @@ User.create(name: 'Vicente Dragicevic H',
 
 puts('Creating groups')
 
-group = Group.create(name: 'Spread Rails',
+group_1 = Group.create(name: 'Spread Rails',
                      description: 'Expandir el imperio de Rails y
                      enfrentar a Django es nuestra labor')
+
+group_2 = Group.create(name: 'Los Cara De Apio',
+                      description: 'Nos gusta comer apio. A veces.')
+
+group_3 = Group.create(name: 'Corazon Cumbia Club',
+                      description: '(8) Mamita mamita rica y apretadita (8)')
 
 puts('Filling groups')
 
 Membership.create(user: User.find(1),
-                  group: group,
+                  group: group_1,
                   is_admin: true)
 
 Membership.create(user: User.find(2),
-                  group: group,
+                  group: group_1,
                   is_admin: false)
+
+Membership.create(user: User.find(5),
+                  group: group_2,
+                  is_admin: true)
+
+Membership.create(user: User.find(4),
+                  group: group_2,
+                  is_admin: false)
+
+Membership.create(user: User.find(1),
+                  group: group_3,
+                  is_admin: true)
+
+Membership.create(user: User.find(2),
+                  group: group_3,
+                  is_admin: true)
 
 puts('Creating places')
 
@@ -74,10 +96,15 @@ place_patiwi = Place.create(owner: User.find(3),
                             description: 'El unico lugar que encontramos',
                             address: 'Vicuña Maquena 3320')
 
-place_dcc = Place.create(owner: group,
+place_dcc = Place.create(owner: group_1,
                          name: 'Sala Javier Pinto',
                          description: 'Algunas veces la prestan, otras no',
-                         address: 'Las Perdices 771')
+                         address: 'Avda. Vicuña Mackenna 4860')
+
+place_fito = Place.create(owner: User.find(2),
+                          name: 'El Fito'
+                          description: 'Restaurante terrible de elegante',
+                          address: 'Vicuña Mackenna 4735')
 
 puts('Creating events')
 
@@ -89,7 +116,7 @@ event_patiwi_1 = Event.create(owner: User.find(4),
                               price: 5000,
                               event_types: [EventType.find(1)])
 
-event_patiwi_2 = Event.create(owner: group,
+event_patiwi_2 = Event.create(owner: group_1,
                               place: place_patiwi,
                               name: 'Musica maestro',
                               description: 'Cumbia en el departamento del Ayudante. Banda de los Jaimes',
@@ -97,13 +124,21 @@ event_patiwi_2 = Event.create(owner: group,
                               price: 100,
                               event_types: [EventType.find(2)])
 
-event_dcc = Event.create(owner: group,
+event_dcc = Event.create(owner: group_1,
                          place: place_dcc,
                          name: 'SQL dificil',
                          description: 'SQL dificil con Rails. No estan invitados los profes del datalab',
                          date: DateTime.strptime('24/04/2015 11:30', '%d/%m/%Y %H:%M'),
                          price: 5000000.1,
                          event_types: [EventType.find(2), EventType.find(3)])
+
+event_tocata = Event.create(owner: group_3,
+                        place: place_fito,
+                        name: 'Tocata pa los vivos',
+                        description: 'Cumbia para alegrar la vida',
+                        date: DateTime.strptime('24/04/2015 11:30', '%d/%m/%Y %H:%M'),
+                        price: 1000,
+                        event_types: [EventType.find(1)])
 
 puts('Creating reports')
 
@@ -116,8 +151,13 @@ puts('Creating comments')
 event_dcc.comments << Comment.create(content: 'DataLab se sentiria avergonzado del polimorfismo',
                                      user: User.find(1))
 
+event_patiwi_1.comments << Comment.create(content: 'Me ******* la vida', user: User.find(5))
+
 event_patiwi_2.comments << Comment.create(content: 'Demasiado bueno para ser verdad. Reportenlo como falso',
                                           user: User.find(2))
+
+event_patiwi_2.comments << Comment.create(content: 'Se viene cabros', user: User.find(5))
+
 
 puts('Creating ratings')
 
