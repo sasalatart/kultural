6130f6e363ac82200ff3a21b63c4c425a5592aa2
Jaime Castro Retamal <jecastro.ie@gmail.com>
2015-06-20 04:22:37 -0300

    Ratings with stars seems to work great

--- kultural/app/views/ratings/create.js
+++ kultural/app/views/ratings/create.js
@@ -1,2 +1,4 @@
 $('#avg-rating').replaceWith("<%= escape_javascript render partial: 'avg_rating', locals: { rateable: @rateable } %>");
-$('#user-rating').replaceWith("<%= escape_javascript render partial: 'form', locals: { rateable: @rateable } %>");
+$('#user-rating').data('url', "<%= url_for([@rateable, @rating]) %>");
+$('#user-rating').data('method', "<%= @rating.value ? 'PATCH' : 'POST' %>");
+$('#user-rating').data('stars', <%= @rating.value || 0 %>);
